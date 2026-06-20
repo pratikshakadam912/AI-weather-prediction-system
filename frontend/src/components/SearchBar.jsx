f995274ca096297bb03231f724f28acbff83cadd
pratikshakadam912 <pratiksha1177@gmail.com>
2026-06-20 13:35:43 +0530

    SearchBar created

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function Searchbar() {
+    return (
+        <div className="search-bar">
+            <input
+                type="text"
+                placeholder="search city..."
+            />
+            <button>🔍</button>
+
+        </div>
+
+    );
+}
+export default SearchBar;
---
+++ frontend/src/components/WeatherCard.jsx
@@ -0,0 +1,14 @@
+function WeatherCard() {
+    return (
+        <div className="waether-card">
+            <h2>Pune</h2>
+            <div className="temprature">
+                31°C
+            </div>
+            <p className="condition">
+                Clear Sky
+            </p>
+        </div>
+    );
+}
+export default WeatherCard;
