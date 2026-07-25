1bc0491976f2a2edc79aa04f95ff57ecc938d1ed
pratikshakadam912 <pratiksha1177@gmail.com>
2026-07-25 13:38:36 +0530

    prediction component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 import History from "./components/History";
-// import AIPrediction from "./components/AIPrediction";
+import AIPrediction from "./components/AIPrediction";
 // import Analytics from "./components/Analytics";
 // import Settings from "./components/Settings";
 
@@ -12,13 +12,16 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/history" element={<History />} />
-
-      {/* Future Pages */}
-      {/* <Route path="/prediction" element={<AIPrediction />} /> */}
-      {/* <Route path="/analytics" element={<Analytics />} /> */}
-      {/* <Route path="/settings" element={<Settings />} /> */}
+      <Route path="/prediction" element={<AIPrediction />} />
     </Routes>
   );
 }
 
 export default App;
+
+{
+  /* <Route path="/analytics" element={<Analytics />} /> */
+}
+{
+  /* <Route path="/settings" element={<Settings />} /> */
+}
